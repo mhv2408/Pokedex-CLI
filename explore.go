8c09610ef0f02f35cb7b173b270cbd5ea944fec9
Harsha Vardhan Mirthinti <mhv2408@gmail.com>
2025-05-07 22:28:35 -0700

    Added the implementation for the explore command

--- explore.go
+++ explore.go
@@ -10,6 +10,7 @@ import (
 func explore(configPtr *config, location string) error {
 
 	location_URL := curr_URL + location
+	fmt.Println(location_URL)
 	res, err := http.Get(location_URL)
 	if err != nil {
 		fmt.Println(err)
@@ -21,14 +22,15 @@ func explore(configPtr *config, location string) error {
 		fmt.Println("Error in decoding the response body ", err)
 		return err
 	}
-	loc := pokemonLocation{}
-	err = json.Unmarshal(body, &loc)
+	location_area := pokemonLocationArea{}
+	err = json.Unmarshal(body, &location_area)
 
 	if err != nil {
 		fmt.Println(err)
 	}
-	for i := 0; i < len(loc.Results); i++ {
-		fmt.Println(loc.Results[i].Name)
+	for i := 0; i < len(location_area.PokemonEncounters); i++ {
+
+		fmt.Println(location_area.PokemonEncounters[i].Pokemon.Name)
 	}
 	return nil
 }
