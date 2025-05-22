e623313a64a626bfbce4fba3ff7197f50276ea11
Harsha Vardhan Mirthinti <mhv2408@gmail.com>
2025-05-22 00:19:59 -0700

    Fixed the catch logic

--- catch.go
+++ catch.go
@@ -14,16 +14,20 @@ func commandCatch(configPtr *config, args ...string) error {
 	if len(args) != 1 {
 		return fmt.Errorf("you must have a pokemon to catch")
 	}
+	if pokemon, ok := configPtr.caughtPokemon[args[0]]; ok {
+		return fmt.Errorf("you have already caught the pokemon: %s", pokemon.Name)
+	}
 	pokemonInfo, err := configPtr.pokeapliClient.CatchPokemon(args[0])
 	if err != nil {
 		return err
 	}
 	fmt.Printf("Throwing a Pokeball at %s...\n", pokemonInfo.Name)
 	if can_catch(pokemonInfo.BaseExperience) {
-		fmt.Printf("%s was caught!\n", pokemonInfo.Name)
+		fmt.Printf("%s was caught!!\n", pokemonInfo.Name)
+		configPtr.caughtPokemon[pokemonInfo.Name] = pokemonInfo
 	} else {
-		fmt.Printf("%s escaped!\n", pokemonInfo.Name)
+		fmt.Printf("%s escaped :(\n", pokemonInfo.Name)
 	}
-	configPtr.caughtPokemon[pokemonInfo.Name] = pokemonInfo
+
 	return nil
 }
